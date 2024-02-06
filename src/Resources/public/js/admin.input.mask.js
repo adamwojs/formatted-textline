c3e32a56d8db7c1f9e1f35a57e4cfa914f333409
Adam Wójs <adam@wojs.pl>
2024-02-06 11:16:50 +0100

    Migration from jquery.mask.js to imask

--- src/Resources/public/js/admin.input.mask.js
+++ src/Resources/public/js/admin.input.mask.js
@@ -1,25 +1,25 @@
-(function (global, doc, eZ, $) {
+(function (global, doc) {
     const SELECTOR_FIELD = `[data-input-mask]`;
     const MASK_PLACEHOLDERS = {
-        '0': {pattern: /[0-9]/},
-        '9': {pattern: /[0-9]/, optional: true},
-        'L': {pattern: /[A-Za-z]/},
-        '?': {pattern: /[A-Za-z]/, optional: true},
-        'A': {pattern: /[A-Za-z0-9]/},
-        'a': {pattern: /[A-Za-z0-9]/, optional: true},
-        'C': {pattern: /[A-Za-z0-9_]/},
-        'c': {pattern: /[A-Za-z0-9_]/, optional: true},
-        'X': {pattern: /[0-9A-Fa-f]/},
-        'x': {pattern: /[0-9A-Fa-f]/, optional: true},
+        '0': /[0-9]/,
+        '9': /[0-9]?/,
+        'L': /[A-Za-z]/,
+        '?': /[A-Za-z]?/,
+        'A': /[A-Za-z0-9]/,
+        'a': /[A-Za-z0-9]?/,
+        'C': /[A-Za-z0-9_]/,
+        'c': /[A-Za-z0-9_]?/,
+        'X': /[0-9A-Fa-f]/,
+        'x': /[0-9A-Fa-f]?/,
     };
 
     doc.querySelectorAll(SELECTOR_FIELD).forEach((input) => {
         const mask = input.dataset.inputMask;
-
         if (mask.length > 0) {
-            $(input).mask(input.dataset.inputMask, {
-                'translation': MASK_PLACEHOLDERS
-            });
+            IMask(input, {
+                mask: mask,
+                definitions: MASK_PLACEHOLDERS
+            })
         }
     });
-})(window, window.document, window.eZ, window.jQuery);
+})(window, window.document);
